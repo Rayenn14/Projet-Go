ba9d7acaa9670adba811acc2dc913c476d17ddf0
SEPT Mg <www.mg1.fr@gmail.com>
2024-11-24 18:59:16 +0100

    maven project

---
+++ projet-go/src/main/java/fr/RyoKaST/App.java
@@ -0,0 +1,13 @@
+package fr.RyoKaST;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
