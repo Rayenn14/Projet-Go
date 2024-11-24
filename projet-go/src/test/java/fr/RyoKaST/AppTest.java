6ec7f37742b187f898be42b3b0eee55f3d9bf4c6
SEPT Mg <www.mg1.fr@gmail.com>
2024-11-24 19:04:48 +0100

    remove

--- projet-go/src/test/java/fr/RyoKaST/AppTest.java
+++ projet-go/src/test/java/fr/RyoKaST/AppTest.java
@@ -8,23 +8,5 @@ import static org.junit.jupiter.api.Assertions.*;
  * Unit test for simple App.
  */
 public class AppTest {
-
-    /**
-     * Basic test to ensure the application runs correctly.
-     */
-    @Test
-    void testApp() {
-        assertTrue(true, "This test should always pass!");
-    }
-
-    /**
-     * Example of a more meaningful test.
-     */
-    @Test
-    void testAddition() {
-        int a = 5;
-        int b = 3;
-        int result = a + b;
-        assertEquals(8, result, "The addition should result in 8");
-    }
+    
 }
