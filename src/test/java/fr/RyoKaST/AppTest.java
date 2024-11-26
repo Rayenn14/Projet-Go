d23b956721646d79e879683d93c421e81c6f5a18
Lukas <lukasstavropoulos94@gmail.com>
2024-11-26 17:49:47 +0100

    Test unitaires de AppTest.java ?!

--- src/test/java/fr/RyoKaST/AppTest.java
+++ src/test/java/fr/RyoKaST/AppTest.java
@@ -8,5 +8,25 @@ import static org.junit.jupiter.api.Assertions.*;
  * Unit test for simple App.
  */
 public class AppTest {
-    
+
+
+    @Test
+    void clearBoard() {
+    }
+
+    @Test
+    void play() {
+    }
+
+    @Test
+    void genmove() {
+    }
+
+    @Test
+    void showBoard() {
+    }
+
+    @Test
+    void testPlay() {
+    }
 }
